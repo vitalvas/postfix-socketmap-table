e4393497b6aac1109769356c4963eef8ffd28a65
Vitaliy Vasilenko <me@vitalvas.com>
2022-10-29 02:18:54 +0300

    reduce goroutine leak

--- handle.go
+++ handle.go
@@ -10,56 +10,56 @@ import (
 func (sm *Server) handle(ctx context.Context, conn net.Conn) error {
 	defer conn.Close()
 
+	ns := netstring.ForReading()
+
 	for {
-		ns := netstring.ForReading()
-
-		for {
-			err := ns.ReadFrom(conn)
-			if err == nil {
-				break
-			}
-			if err == netstring.Incomplete {
-				continue
-			}
-
-			return err
+		err := ns.ReadFrom(conn)
+		if err == nil {
+			break
+		}
+		if err == netstring.Incomplete {
+			continue
 		}
 
-		r := &Request{}
-		if err := r.Decode(ns); err != nil {
-			return err
-		}
-
-		sm.mapsLock.RLock()
-		fn, exists := sm.maps[r.Name]
-		sm.mapsLock.RUnlock()
-
-		var res *Result
-		var err error
-
-		if exists {
-			res, err = fn(ctx, r.Key)
-			if err != nil {
-				res = ReplyTempFail(err.Error())
-			}
-
-		} else if sm.defaultMap != nil {
-			res, err = sm.defaultMap(ctx, r.Name, r.Key)
-			if err != nil {
-				res = ReplyTempFail(err.Error())
-			}
-
-		} else {
-			res = ReplyTempFail("the lookup map does not exist")
-		}
-
-		b, err := res.Encode().Marshal()
-		if err != nil {
-			return err
-		}
-
-		if _, err := conn.Write(b); err != nil {
-			return err
-		}
+		return err
 	}
+
+	r := &Request{}
+	if err := r.Decode(ns); err != nil {
+		return err
+	}
+
+	sm.mapsLock.RLock()
+	fn, exists := sm.maps[r.Name]
+	sm.mapsLock.RUnlock()
+
+	var res *Result
+	var err error
+
+	if exists {
+		res, err = fn(ctx, r.Key)
+		if err != nil {
+			res = ReplyTempFail(err.Error())
+		}
+
+	} else if sm.defaultMap != nil {
+		res, err = sm.defaultMap(ctx, r.Name, r.Key)
+		if err != nil {
+			res = ReplyTempFail(err.Error())
+		}
+
+	} else {
+		res = ReplyTempFail("the lookup map does not exist")
+	}
+
+	b, err := res.Encode().Marshal()
+	if err != nil {
+		return err
+	}
+
+	if _, err := conn.Write(b); err != nil {
+		return err
+	}
+
+	return nil
 }
